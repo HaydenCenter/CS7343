a97b5a8c1c9f596033f9ef86718fe528aca0cac8
Hayden Center (HC-HPS Ubuntu) <haydencenter72@gmail.com>
2021-10-26 20:28:14 -0500

    Finished stage 1: commenting and minor logic fix

--- Program2/stage_1.c
+++ Program2/stage_1.c
@@ -20,18 +20,18 @@ struct Queue {
 int N; /* Number of slots */
 int T; /* Number of objects */
 int p; /* Number of players */
-int start = 0;
-int end = 0;
-int threads = 0;
-int nextPlayer;
-int *scores; /* Array of scores for each thread */
-char **names;
+int start = 0; /* Signal to start each game */
+int end = 0; /* Signal to end the whole game */
+int threads = 0; /* Number of threads running */
+int nextPlayer; /* Index of slot for next player to fill */
+int *scores; /* Array of scores of the player in each slot */
+char **names; /* Array of names of the player in each slot */
 struct Queue* q; /* FIFO queue */
 pthread_mutex_t queueLock; /* Mutex for FIFO queue */
 
 
-int game = 0; /* Count of players still playing the 
-              |* game (including dealer). Ensures that
+int game = 0; /* Count of players still playing the game
+              |* (including dealer & queue). Ensures that
               |* the game does not end until the dealer
               |* has stopped generating numbers and all
               |* players have emptied their hands. */
@@ -63,13 +63,13 @@ void push(int x, int print) {
     q->size += 1;
     if(q->size == 1) {
         q->tail = q->head;
-        /* Critical section H is necessary to keep the threads from thinking the
+        /* Critical section "push" is necessary to keep the threads from thinking the
         |* is finished until the queue is empty, the dealer is done generating,
         |* and the threads are not holding a popped number. This section incrememnts
         |* the game counter if the queue was empty and gets pushed to. */
-        pthread_mutex_lock(&gameLock); /* Begin critical section H */
+        pthread_mutex_lock(&gameLock); /* Begin critical section "push" */
         game++;
-        pthread_mutex_unlock(&gameLock); /* End critical section H */
+        pthread_mutex_unlock(&gameLock); /* End critical section "push" */
     }
     if(print)
         printQueue();
@@ -83,13 +83,13 @@ int pop(int print) {
     q->tail = tail->prev;
     if(q->size == 1) {
         q->head = NULL;
-        /* Critical section I is necessary to keep the threads from thinking the
+        /* Critical section "pop" is necessary to keep the threads from thinking the
         |* is finished until the queue is empty, the dealer is done generating,
         |* and the threads are not holding a popped number. This section decrements
         |* the game counter if the queue will be empty after getting popped. */
-        pthread_mutex_lock(&gameLock); /* Begin critical section I */
+        pthread_mutex_lock(&gameLock); /* Begin critical section "pop" */
         game--;
-        pthread_mutex_unlock(&gameLock);  /* End critical section I */
+        pthread_mutex_unlock(&gameLock);  /* End critical section "pop" */
     } else {
         q->tail->next = NULL;
     }
@@ -100,6 +100,7 @@ int pop(int print) {
     return result;
 }
 
+/* Resets the scores array and clears the queue */
 void resetGame(int print) {
     if(print)
         printf("Resetting scores...\n");
@@ -107,7 +108,7 @@ void resetGame(int print) {
         scores[i] = 0;
     }
 
-    pthread_mutex_lock(&gameLock);
+    pthread_mutex_lock(&queueLock);
     if(print)
         printf("Clearing queue...\n");
     while(q->size > 0) {
@@ -115,7 +116,7 @@ void resetGame(int print) {
     }
     if(print)
         printQueue();
-    pthread_mutex_unlock(&gameLock);
+    pthread_mutex_unlock(&queueLock);
 }
 
 /* Function passed to pthreads */
@@ -146,24 +147,28 @@ int main(int argc, char *argv[])
 
     printf("Number of threads : %d | Number of objects : %d\n\n", N, T);
 
+    /* Initializes queue */
     q = malloc(sizeof(struct Queue));
     q->size = 0;
     q->head = NULL;
     q->tail = NULL;
 
+    /* Initialize queue lock */
     if (pthread_mutex_init(&queueLock, NULL) != 0) {
         printf("ERROR: Queue mutex initialization has failed\n");
         return -1;
     }
 
+    /* Initialize game lock */
     if (pthread_mutex_init(&gameLock, NULL) != 0) {
         printf("ERROR: Game mutex initialization has failed\n");
         return -1;
     }
     
+    /* Initialize scores and names arrays */
     scores = malloc(sizeof(int) * N);
     names = malloc(sizeof(char*) * p);
-    resetGame(0);
+    resetGame(0); /* Initializes scores to 0 */
 
     /* Create all threads */
     pthread_t tid[N];
@@ -172,16 +177,19 @@ int main(int argc, char *argv[])
 
     for(int i = 0; i < N; i++)
     {
+        names[i] = players[i]; /* Assigns first N players to N slots */
         int *player = malloc(sizeof(int));
         *player = i;
-        names[i] = players[i];
         pthread_create(&(tid[i]), &attr, runner, (void *) player);
     }
 
+    /* Sets the index of the next player into the game */
     nextPlayer = N;
 
+    /* Waits until all N threads are ready */
     while(threads < N);
 
+    /* Loops until game is signaled to end */
     while(!end) {
         printf("\n"
         "Players\n"
@@ -194,9 +202,9 @@ int main(int argc, char *argv[])
         "--------------\n");
 
         /* Signal the game to start */
-        pthread_mutex_lock(&gameLock); /* Begin critical section A after creating all threads */
+        pthread_mutex_lock(&gameLock); /* Begin critical section "generating" after creating all threads */
         game++;
-        pthread_mutex_unlock(&gameLock); /* End critical section A after making sure the game will continue */
+        pthread_mutex_unlock(&gameLock); /* End critical section "generating" after making sure the game will continue */
         start = 1;
 
         /* Repeat until T numbers generated */
@@ -204,23 +212,24 @@ int main(int argc, char *argv[])
         int numbers = T;
         while(numbers) {
             /* Generate a new number if queue has N or fewer numbers */
-            pthread_mutex_lock(&queueLock); /* Begin critical section B to push a number to the queue */
+            pthread_mutex_lock(&queueLock); /* Begin critical section "deal" to push a number to the queue */
             if(q->size <= N) {
                 int x = rand() % 40;
                 printf("Dealer is pushing %d to the queue\n", x);
                 push(x, 1);
                 numbers--;
             }
-            pthread_mutex_unlock(&queueLock); /* End critical section B after the number has been pushed */
+            pthread_mutex_unlock(&queueLock); /* End critical section "deal" after the number has been pushed */
         }
 
         printf("Done generating\n");
 
-        pthread_mutex_lock(&gameLock); /* Begin critical section C after generating all T numbers */
+        pthread_mutex_lock(&gameLock); /* Begin critical section "done" after generating all T numbers */
         game--;
-        pthread_mutex_unlock(&gameLock); /* End critical section C after ending the main process's need for the game to be running */
+        pthread_mutex_unlock(&gameLock); /* End critical section "done" after ending the main process's need for the game to be running */
 
         while(game); /* Busy waiting */
+        start = 0; /* Make threads wait at the end of the game */
 
         /* Print the final score for each player */
         int max = -1;
@@ -235,16 +244,18 @@ int main(int argc, char *argv[])
 
         printf("\nWinner: %s, with %d points\n", names[winner], scores[winner]);
 
+        /* If the index of the next player is less than the
+        |* total number of players, reset and continue */
         if(nextPlayer < p) {
             resetGame(1);
-            names[winner] = players[nextPlayer];
-            nextPlayer++;
-            max = -1;
-            winner = -1;
+            names[winner] = players[nextPlayer]; /* Replace the winner with the next player */
+            nextPlayer++; /* Increment the index of the new next player */
+            max = -1; /* Reset the highest score from the game */
+            winner = -1; /* Reset who the winner is */
         } else {
-            end = 1;
+            end = 1; /* End the game when out of players */
         }
-        start = 1;
+        start = 1; /* End the busy wait at the end of each round */
     }
 
     /* Wait for all threads to finish */
@@ -265,14 +276,15 @@ void *runner(void *param) {
 
     /* Repeat until the game ends */
     while(!end) {
+        /* Continue while there are still numbers in play in the dealer or players hands or in the queue */
         while(game) {
-            pthread_mutex_lock(&queueLock); /* Begin critical section D while game is still running */
+            pthread_mutex_lock(&queueLock); /* Begin critical section "grab" while game is still running */
             /* If the queue is not empty */
             if(q->size > 0) {
                 /* Get the next number */
-                pthread_mutex_lock(&gameLock); /* Begin critical section E if there are numbers in the queue */
+                pthread_mutex_lock(&gameLock); /* Begin critical section "hold" if there are numbers in the queue */
                 game++;
-                pthread_mutex_unlock(&gameLock); /* End critical section E after making sure the game will continue */
+                pthread_mutex_unlock(&gameLock); /* End critical section "hold" after making sure the game will continue */
                 int x = pop(0);
                 int score;
                 int result;
@@ -289,7 +301,7 @@ void *runner(void *param) {
                 printQueue();
 
                 /* Sleep for 1 + x ms */
-                pthread_mutex_unlock(&queueLock); /* End critical section D after a number has been taken from the queue and saved */
+                pthread_mutex_unlock(&queueLock); /* End critical section "grab" after a number has been taken from the queue and saved */
                 struct timespec req, rem;
                 req.tv_sec = 0;
                 req.tv_nsec = x * 1000000 + 1;
@@ -300,24 +312,24 @@ void *runner(void *param) {
                     scores[k] += score;
                 } else if((x % N == k) || (x % N == (k + 1) % N)) {
                     scores[k] += score;
-                    pthread_mutex_lock(&queueLock); /* Begin critical section F1 if a number needs to be pushed to the queue */
+                    pthread_mutex_lock(&queueLock); /* Begin critical section "matched" if a number needs to be pushed to the queue */
                     printf("Thread %d is pushing %d to the queue\n", k, x - score);
                     push(x - score, 1);
-                    pthread_mutex_unlock(&queueLock); /* End critical section F1 after the number has been pushed */
+                    pthread_mutex_unlock(&queueLock); /* End critical section "matched" after the number has been pushed */
                 } else {
-                    pthread_mutex_lock(&queueLock); /* Begin critical section F2 if a number needs to be pushed to the queue */
+                    pthread_mutex_lock(&queueLock); /* Begin critical section "fail" if a number needs to be pushed to the queue */
                     printf("Thread %d is pushing %d to the queue\n", k, x - 2);
                     push(x - 2, 1);
-                    pthread_mutex_unlock(&queueLock); /* End critical section F2 after the number has been pushed */
+                    pthread_mutex_unlock(&queueLock); /* End critical section "fail" after the number has been pushed */
                 }
-                pthread_mutex_lock(&gameLock); /* Begin critical section G after dealing with the object */
+                pthread_mutex_lock(&gameLock); /* Begin critical section "release" after dealing with the object */
                 game--;
-                pthread_mutex_unlock(&gameLock); /* End critical section G after ending the threads need for the game to be running */
+                pthread_mutex_unlock(&gameLock); /* End critical section "release" after ending the threads need for the game to be running */
             } else {
-                pthread_mutex_unlock(&queueLock); /* End critical section D if there is no number to take from the queue */
+                pthread_mutex_unlock(&queueLock); /* End critical section "grab" if there is no number to take from the queue */
             }
 
-            while(!start);
+            while(!start); /* Busy waiting at the end of each game */
         }
     }
 }
